6bfcdf1365da42f321fd5e2c681910049ba3ee62
Jacob Earle <earlejacobt@gmail.com>
2022-08-01 17:46:23 -0500

    Added favicon

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,15 +1,9 @@
-import Head from 'next/head';
 import React from 'react';
 import Navbar from './Navbar';
 
 const Layout = ({children}: {children: React.ReactNode}) => {
     return (
         <>
-            <Head>
-                <title>They Could've Met</title>
-                <meta name="description" content="Discover who could've met." />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
             <Navbar />
             <main>{children}</main>
         </>
